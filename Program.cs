9bb39445a17ecdda5575f7f4f6284c9f2fb307a0
Shishikyuu <richard.febres@gmail.com>
2021-02-02 21:39:05 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first name.");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Please enter your last name.");
+            string lastName = Console.ReadLine();
+            Console.WriteLine($"Hi,  {firstName} {lastName}");
+        }
+    }
+}
