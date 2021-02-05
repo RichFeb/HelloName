0c718354100b4ac74e593500820c5a8ff7a98b83
Shishikyuu <richard.febres@gmail.com>
2021-02-05 18:03:53 -0500

    Add README.md and add img of build to assets folder

--- Program.cs
+++ Program.cs
@@ -4,13 +4,20 @@ namespace HelloName
 {
     class Program
     {
+           
         static void Main(string[] args)
         {
+       
             Console.WriteLine("Please enter your first name.");
+            
             string firstName = Console.ReadLine();
+            
             Console.WriteLine("Please enter your last name.");
+            
             string lastName = Console.ReadLine();
-            Console.WriteLine($"Hi,  {firstName} {lastName}");
+            
+            Console.WriteLine($"Hi, Rich Febres");
+           
         }
     }
 }
